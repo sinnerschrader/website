df52b42f2ba581f61bc3a785bdc696bad805052f
Mario Nebl <hello@herebecode.com>
2016-12-01 22:28:41 +0100

    chore: use same version of skip-ci with build entry script

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [[ "$(git log --format=%s -n 1 $TRAVIS_COMMIT)" == *"[skip-ci]"* ]]; then
+if [[ "$(git log --format=%s -n 1)" == *"[skip-ci]"* ]]; then
 	echo "Commit subject ends with \"[skip-ci]\", skipping."
 	exit 0
 fi
