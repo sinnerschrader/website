8eb2c37a606ef5f6cf54f4e4e3a490aed4ec9c3e
Felix Scheffel <felix.scheffel@sinnerschrader.com>
2017-04-07 11:48:30 +0200

    chore: remove prepare static folder tasks for icons, images and videos

--- scripts/prepare-static.sh
+++ scripts/prepare-static.sh
@@ -4,9 +4,6 @@ set -e
 
 if [ ! -d node_modules/sinnerschrader-website-static ] ; then
 	mkdir -p node_modules/sinnerschrader-website-static/statics/fonts
-	mkdir -p node_modules/sinnerschrader-website-static/statics/icons
-	mkdir -p node_modules/sinnerschrader-website-static/statics/images
-	mkdir -p node_modules/sinnerschrader-website-static/statics/videos
 	echo '{"name": "website-static", "version": "0.0.1"}' > node_modules/website-static/package.json
 	echo 'const path = require("path");module.exports = path.join(__dirname, "statics");'  > node_modules/website-static/index.js
 fi
