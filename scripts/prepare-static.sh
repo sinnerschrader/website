88cf63e85778bb73d4bbc95bbfb7f03af8c32925
Mario Nebl <mario.nebl@sinnerschrader.com>
2017-05-15 14:44:23 +0200

    chore: use an external fonts stylesheet

--- scripts/prepare-static.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-
-if [ ! -d node_modules/sinnerschrader-website-static ] ; then
-	mkdir -p node_modules/sinnerschrader-website-static/statics/fonts
-	echo '{"name": "website-static", "version": "0.0.1"}' > node_modules/website-static/package.json
-	echo 'const path = require("path");module.exports = path.join(__dirname, "statics");'  > node_modules/website-static/index.js
-fi
