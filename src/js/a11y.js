9faee92472246ec42f88a038b04eaf46fc90a25b
Alex Peschel <alexanderpeschel@me.com>
2017-06-12 11:17:46 +0200

    refactor: javascript rewritten to make it reusable, change navigation button js- classname to an reusable classname

--- src/js/a11y.js
+++ src/js/a11y.js
@@ -1,32 +1,97 @@
-const navigationState = document.querySelector('#navigation-state');
-const navigationButton = document.querySelector('.js-burger');
+initAriaExpandedTriggers();
+initTabLists();
 
-navigationButton.setAttribute('aria-expanded', 'false');
+function initAriaExpandedTriggers() {
+ const ariaExpandedTriggers = nodeListToArray(document.querySelectorAll('.js-aria-expanded'));
 
-navigationButton.addEventListener('click', function(e){
-    navigationButton.setAttribute('aria-expanded', navigationButton.getAttribute('aria-expanded') === 'false' ? 'true' : 'false');
-}); 
+ ariaExpandedTriggers.forEach((trigger) => {
+   /**
+    * set aria-expanend initial
+    * this is needed to ensure that the page
+    * is accesable without javascript
+    */
+   setElementAttribute(trigger, 'aria-expanded', false);
 
-document.addEventListener('keydown', function(e){
-    if(e.keyCode === 27) { // ESC
-        navigationState.checked = false;
-        navigationButton.setAttribute('aria-expanded', 'false');
-        navigationButton.focus();
-    }
-});
+   /** 
+    * toggle aria expanded attribute for current target element
+    */
+   trigger.addEventListener('click', function() {
+       if(trigger.getAttribute('aria-expanded') === 'false') {
+           setElementAttribute(trigger, 'aria-expanded', true);
+       } else {
+           setElementAttribute(trigger, 'aria-expanded', false);
+       }
+   });
+ });
 
+ /** 
+  * if keycode is equal to escape key
+  * set checkbox state false
+  * set aria expanded false
+  * and set focus to trigger element
+  */ 
+ document.addEventListener('keydown', function(event) {
+     if(event.keyCode === 27) {
+       ariaExpandedTriggers.forEach((trigger) => {
+         const triggerCheckbox = trigger.previousSibling;
 
-const tabs = document.querySelectorAll('[role="tab"]');
-console.log(document);
-// set first tab to aria-selected true
-for (var i=0; i < tabs.length; i++) {
-    tabs[i].addEventListener('change', function(){                         
-        this.parentElement.querySelectorAll('[role="tab"]').forEach.call(tabs, function(tab) {
-            if(tab.checked) {
-                tab.setAttribute('aria-selected', 'true');
-            } else {
-                tab.removeAttribute('aria-selected');
-            }
-        });
-    });
+         triggerCheckbox.checked = false;
+         trigger.setAttribute('aria-expanded', 'false');
+         trigger.focus();
+       });
+     }
+ });
+}
+
+function initTabLists() {
+ const tabLists = document.querySelectorAll('[role="tablist"]');
+
+ /** 
+  * set target element aria selected attribute true
+  * and set sibling aria selected attribute false
+  */ 
+ nodeListToArray(tabLists).forEach((tabList) => {
+   const tabs = nodeListToArray(tabList.querySelectorAll('[role="tab"]'));
+   
+   tabs.forEach((tab) => {
+     tab.addEventListener('click', function() {
+       setElementAttribute(tabs, 'aria-selected', false);
+       setElementAttribute(tab, 'aria-selected', true);
+     })
+   });
+ });  
+}
+
+/** 
+* convert a nodeList to an array of nodes
+*/
+function nodeListToArray(nodeList) {
+ const returnValue = [];
+
+ for (let i = 0; i < nodeList.length; i++) {
+   returnValue.push(nodeList.item(i));
+ }
+
+ return returnValue;
+}
+
+/**
+* set attribute to element /element Array
+* 
+* @param element - target element or element Array
+* @param attribute - target element attribute
+* @param value - attribute value
+* 
+*/
+function setElementAttribute(element, attribute, value) {
+  if (element instanceof Element) {
+     element.setAttribute(attribute, value);
+     return;
+ }
+
+ if (element instanceof Array) {
+   element.forEach((currentElement) => {
+     currentElement.setAttribute(attribute, value);
+   });
+ }
 }
