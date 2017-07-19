b51e8f458c1240d309b4d0e48cc83f28724e1731
Felix Scheffel <felix.scheffel@sinnerschrader.com>
2017-07-19 12:20:14 +0200

    feat: comment intersection video script

--- src/js/intersection-video.js
+++ src/js/intersection-video.js
@@ -1,9 +1,17 @@
 const observerTarget = document.querySelector('.intersection-video video');
 
+/**
+ * if video is defined
+ * create intersection observer
+ */
 if(observerTarget) {
+
+	/**
+	 * threshold is a single number or an array of numbers which indicate
+	 * at what percentage of the target's visibility the observer's
+	 * callback should be executed.
+	 */
 	const observerOptions = {
-		root: null,
-		rootMargin: '0px',
 		threshold: [0.5,0.75,1.0]
 	}
 
@@ -13,6 +21,11 @@ if(observerTarget) {
 
 function observerCallback(entries, observer) {
 		entries.forEach(entry => {
+
+		/**
+		 * Whenever the target meets a threshold
+		 * >= 75% play the video else pause video
+		 */
 		if(entry.intersectionRatio>=.75){
 			entry.target.play();
 		} else {
