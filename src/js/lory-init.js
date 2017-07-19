80eb1d4d568c881a4d15e4b0a0ac8e8d5fc7e972
Felix Scheffel <felix.scheffel@sinnerschrader.com>
2017-07-19 15:05:16 +0200

    feat: kiekmo lory comments & change hero image

--- src/js/lory-init.js
+++ src/js/lory-init.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var simple_dots       = document.querySelector('.js_simple_dots');
 
+	/**
+	 * if slider is not on the page return
+	 */
 	if(!simple_dots) {
 		return;
 	}
@@ -9,6 +12,10 @@ document.addEventListener('DOMContentLoaded', function () {
 	var dot_container     = simple_dots.querySelector('.js_dots');
 	var dot_list_item     = document.createElement('li');
 
+	/**
+	 * dot naviagtion init function
+	 * @param {*} e
+	 */
 	function handleDotEvent(e) {
 		if (e.type === 'before.lory.init') {
 			for (var i = 0, len = dot_count; i < len; i++) {
@@ -37,11 +44,15 @@ document.addEventListener('DOMContentLoaded', function () {
 			dot_container.childNodes[0].classList.add('active');
 		}
 	}
+
 	simple_dots.addEventListener('before.lory.init', handleDotEvent);
 	simple_dots.addEventListener('after.lory.init', handleDotEvent);
 	simple_dots.addEventListener('after.lory.slide', handleDotEvent);
 	simple_dots.addEventListener('on.lory.resize', handleDotEvent);
 
+	/**
+	 * lory dot slider options
+	 */
 	var dot_navigation_slider = lory(simple_dots, {
 		infinite: 1,
 		enableMouseEvents: true
