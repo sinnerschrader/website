f8e5c233e647361fe40cf7321f17620cfddb9308
Felix Scheffel <felix.scheffel@sinnerschrader.com>
2017-08-03 12:03:20 +0200

    feat: refactor js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,22 @@
 const intersectionVideo = require('./intersection-video');
 const slider = require('./slider');
+const initAriaExpanded = require('./init-aria-expanded');
+const initTabList = require('./init-tab-list');
 
 window.addEventListener('DOMContentLoaded', main);
 
 function main() {
-	const intersected = query('.intersection-video video')
+	const intersected = query('[data-js="intersection-video"] video')
 		.map(el => intersectionVideo(el));
 
-	const sliders = query('.js_simple_dots')
+	const sliders = query('[data-js="simple-dots"]')
 		.map(el => slider(el));
+
+	const ariaExpandedList = query('[data-js="aria-expanded"]')
+		.map(el => initAriaExpanded(el));
+
+	const tabs = query('[role="tablist"]')
+		.map(el => initTabList(el));
 }
 
 function query(q) {
