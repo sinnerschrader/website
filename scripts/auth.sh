d3a7914ccb43fb9095af8a2901629921f62fff09
Feli <felicitas.kugland@sinnerschrader.com>
2020-02-12 15:03:44 +0100

    Change Hubot

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-git config --global user.email "patternplate-hubot@sinnerschrader.com"
-git config --global user.name "Patternplate Hubot"
+git config --global user.email "s2-github-bot@sinnerschrader.com"
+git config --global user.name "S2 Github Bot"
 
 $(npm bin)/set-up-ssh --key "$encrypted_e0af519562d0_key" \
 											--iv "$encrypted_e0af519562d0_iv" \
