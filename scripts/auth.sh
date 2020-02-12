d25ed70270b3e7956d0b11baa411a075a229bc21
Feli <felicitas.kugland@sinnerschrader.com>
2020-02-12 15:48:43 +0100

    update encrypted key names and path

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -4,6 +4,6 @@ set -e
 git config --global user.email "s2-github-bot@sinnerschrader.com"
 git config --global user.name "S2 Github Bot"
 
-$(npm bin)/set-up-ssh --key "$encrypted_e0af519562d0_key" \
-											--iv "$encrypted_e0af519562d0_iv" \
-											--path-encrypted-key ".travis/patternplate-hubot.enc"
+$(npm bin)/set-up-ssh --key "$encrypted_b93e83ba6e4c_key" \
+		      --iv "$encrypted_b93e83ba6e4c_iv" \
+		      --path-encrypted-key ".travis/github_deploy_key.enc"
