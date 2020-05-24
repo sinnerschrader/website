9d2666e57027c5f76213cab80fd78c015c0bb73c
David Durlan <david.a.durlan@gmail.com>
2020-05-24 16:05:06 +0200

    fix burger menu keyboard support

--- src/js/init-aria-expanded.js
+++ src/js/init-aria-expanded.js
@@ -4,32 +4,51 @@ module.exports = function initAriaExpanded(el) {
 	 * this is needed to ensure that the page
 	 * is accesable without javascript
 	 */
-	el.setAttribute('aria-expanded', false);
+	el.setAttribute("aria-expanded", false);
 
 	/**
 	 * toggle aria expanded attribute for current target element
 	 */
-	el.addEventListener('click', function () {
-		if (el.getAttribute('aria-expanded') === 'false') {
-			el.setAttribute('aria-expanded', true);
+	el.addEventListener("click", toggleAriaExpanded);
+	function toggleAriaExpanded() {
+		if (el.getAttribute("aria-expanded") === "false") {
+			el.setAttribute("aria-expanded", true);
 		} else {
-			el.setAttribute('aria-expanded', false);
+			el.setAttribute("aria-expanded", false);
 		}
-	});
+	}
 
 	/**
-	 * if keycode is equal to escape key
-	 * set checkbox state false
-	 * set aria expanded false
-	 * and set focus to trigger element
+	 * control aria expanded attribute and checkbox checked attribute with enter and escape key
 	 */
-	document.addEventListener('keydown', function (event) {
-		if (event.keyCode === 27) {
-			const elCheckbox = el.previousSibling;
+	function additionalKeys() {
+		const elCheckbox = el.previousSibling;
 
-			triggerCheckbox.checked = false;
-			el.setAttribute('aria-expanded', 'false');
-			el.focus();
-		}
-	});
-}
+		/**
+		 * if keycode is equal to enter key
+		 * toggle checkbox
+		 * toggle aria expanded
+		 */
+		elCheckbox.addEventListener("keydown", function (event) {
+			if (event.keyCode === 13) {
+				elCheckbox.checked = !elCheckbox.checked;
+				toggleAriaExpanded();
+			}
+		});
+
+		/**
+		 * if keycode is equal to escape key
+		 * set checkbox state false
+		 * set aria expanded false
+		 * and set focus to trigger element
+		 */
+		document.addEventListener("keydown", function (event) {
+			if (event.keyCode === 27) {
+				elCheckbox.checked = false;
+				el.setAttribute("aria-expanded", "false");
+				el.focus();
+			}
+		});
+	}
+	additionalKeys();
+};
